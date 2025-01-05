ba43f92ec2d4c5d220950bfcfc0816da05802e01
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-01-05 12:57:53 +0800

    V0.1.6.1 完成用户权限初始化脚本

---
+++ env_init/src/init_host.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+r='\033[31m'
+g='\033[32m'
+b='\033[34m'
+n='\033[0m'
+
+data_dir=$1
+
+echo -e "${b}------------------- 用户权限初始化 -------------------${n}"
+
+if [ -z "$data_dir" ]; then echo -e "${r}错误: data_dir 未设置。${n}"; exit 1; fi
+
+if ! getent group whooshing > /dev/null; then groupadd whooshing; echo -e "${g}组 'whooshing' 已创建。${n}"
+else echo -e "${g}组 'whooshing' 已存在。${n}"; fi
+
+if ! id -u woo > /dev/null 2>&1; then useradd -m -g woo woo; usermod -aG whooshing woo; echo -e "${g}用户 'woo' 已创建并添加到 'whooshing' 组。${n}"
+else usermod -aG whooshing woo; echo -e "${g}用户 'woo' 已存在并添加到 'whooshing' 组。${n}"; fi
+
+if [ ! -d "$data_dir" ]; then mkdir -p "$data_dir"; fi
+
+usermod -s /bin/bash woo
+usermod -aG whooshing root
+sudo chown -R root:whooshing "$data_dir"
+chmod -R 770 "$data_dir"
+echo -e "${g}数据目录 '$data_dir' 的所有权已更改为 whooshing 组，且权限设置完成。${n}"
+
+echo -e "${b}------------------- 用户权限初始化完成 -------------------${n}"
