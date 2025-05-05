cc0691baffb7cda8a98c4f28f999405332fa9336
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-05 09:51:44 +0800

    V0.1.10.1 Update init_nginx.sh

--- env_init/src/init_nginx.sh
+++ env_init/src/init_nginx.sh
@@ -17,13 +17,8 @@ if command -v nginx >/dev/null 2>&1; then
 else
     # 更新包管理器并安装 Nginx
     echo -e "${b}更新包管理器...${n}"
-    sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
-    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-    gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
-
-    echo -e "${b}安装 Nginx...${n}"
     sudo apt update
+    echo -e "${b}安装 Nginx...${n}"
     sudo apt install -y nginx
 fi
 
