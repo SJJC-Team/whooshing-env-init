abd90a59515da08523f5df91755801b659c6f011
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-05-05 10:23:54 +0800

    V0.1.10.6 设置清理程序

--- env_init/init.sh
+++ env_init/init.sh
@@ -9,6 +9,16 @@ n='\033[0m'
 
 data_dir=${1:-/whooshing}
 
+# 定义清理函数
+cleanup() {
+    echo -e "${b}清理...${n}"
+    sudo rm -rf /root/.whooshing
+    echo -e "${g}清理完成.${n}"
+}
+
+# 设置 trap 捕获 EXIT 信号，确保清理函数总会执行
+trap cleanup EXIT
+
 # 授权 src 文件夹中的所有 sh 文件
 echo -e "${b}正在授权 src 文件夹中的所有 sh 文件...${n}"
 find "$(dirname "$0")" -type f -name "*.sh" -exec chmod +x {} \;
@@ -27,8 +37,5 @@ sudo src/uninstall.sh
 
 echo -e "${b}运行初始化程序...${n}"
 sudo src/init_all.sh $data_dir
-
-echo -e "${b}清理...${n}"
-cd /root; sudo rm -rf /root/.whooshing
 
 echo -e "${g}环境初始化完成.${n}"
